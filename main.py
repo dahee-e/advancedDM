d48b13e20dbc2706378e368f2609fd9d8d299f45
dahee <dahee@unist.ac.kr>
2024-06-09 16:14:05 +0900

    :art: Improve format of the code.

--- main.py
+++ main.py
@@ -37,39 +37,14 @@ cmty_diamter = None
 
 
 
-
-cmty_etimate_result= {
-    'TC1-1': 137,
-    'TC1-2': 151,
-    'TC1-3': 171,
-    'TC1-4': 186,
-    'TC1-5': 209,
-    'TC1-6': 230,
-    'TC1-7': 256,
-    'TC1-8': 296,
-    'TC1-9': 294,
-    'TC1-10': 294,
-    'karate': 2,
-    'dolphin': 3,
-    'football': 8,
-    'railway': 19,
-    'polbooks': 5,
-    'strike': 2,
-    'mexican': 3
-}
-
-
 G = nx.read_edgelist(args.network)
 G.remove_edges_from(nx.selfloop_edges(G))
 
-estimate_communities = utils.get_estimated_value(args.network, cmty_etimate_result)
-if estimate_communities is None:
-    estimate_communities = args.estimateCmty
 
 print("network:\t", args.network)
 print("entire graph V=", G.number_of_nodes(), "\tE=", G.number_of_edges())
 print("approach:\t", args.approach)
-print("num-community:\t", estimate_communities)
+print("num-community:\t", args.estimateCmty)
 if args.approach == "seednode":
     print("centrality measures to use:\t", args.centrality)
 else:
@@ -82,7 +57,7 @@ if args.approach == "seednode":
               centrality={centrality_measure: {} for centrality_measure in args.centrality.split(",")})
 
     start_time = time.time()
-    detected_communities,cmty_diameter = CDS.run(G, estimate_communities)
+    detected_communities,cmty_diameter = CDS.run(G, args.estimateCmty)
     end_time = time.time()
     detected_communities = list(detected_communities.values())
     G = G.graph
@@ -95,9 +70,9 @@ elif args.approach == "seedsubgraph":
     start_time = time.time()
 
     if args.model == 'kcore':
-        seed_subgraph = kcore.run(G, estimate_communities, utils.get_base(args.network))
+        seed_subgraph = kcore.run(G, args.estimateCmty, utils.get_base(args.network))
     elif args.model == 'ktruss':
-        seed_subgraph = ktruss.run(G, estimate_communities, utils.get_base(args.network))
+        seed_subgraph = ktruss.run(G, args.estimateCmty, utils.get_base(args.network))
 
     # label propagation with nodes in G - seed_subgraph
     detected_communities = LP.label_propagation(G, seed_subgraph)
@@ -115,7 +90,7 @@ with open(utils.get_base(args.network) + args.approach + "_result.txt", 'w') as 
     f.write("Network:\t" + args.network + "\n")
     f.write("entire graph V=" + str(G.number_of_nodes()) + "\tE=" + str(G.number_of_edges()) + "\n")
     f.write("Time:\t" + str(end_time - start_time) + "\n")
-    f.write("estimate community:\t" + str(estimate_communities) + "\n")
+    f.write("estimate community:\t" + str(args.estimateCmty) + "\n")
     if args.approach == "seednode":
         f.write("approximated diamter:\t" + str(cmty_diamter) + "\n")
     if args.approach == "seedsubgraph":
@@ -128,7 +103,7 @@ with open(utils.get_base(args.network) + args.approach + "_result.txt", 'w') as 
 #print result
 print("------------RESULT------------")
 print("Time:\t", end_time - start_time)
-print("estimate community:\t", estimate_communities)
+print("estimate community:\t", args.estimateCmty)
 if args.approach == "seednode":
     print("approximated diamter:\t", cmty_diamter)
 print("nmi_score:\t", nmi_score)
